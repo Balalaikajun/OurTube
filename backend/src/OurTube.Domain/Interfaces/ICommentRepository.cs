8f117eb456f26cc1f247c6a87c7c7f61db7f6a7d
Andreu K.P. <andrejkluev774@gmail.com>
2025-06-06 12:03:46 +0700

    Delete interfaces of repositories

--- backend/src/OurTube.Domain/Interfaces/ICommentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OurTube.Domain.Entities;
-
-namespace OurTube.Domain.Interfaces
-{
-    public interface ICommentRepository : IRepository<Comment>
-    {
-        Task<IEnumerable<Comment>> GetWithLimitAsync(int videoId, int limit, int after, int? parentId = null);
-    }
-}
--- backend/src/OurTube.Domain/Interfaces/ITagRepository.cs
+++
@@ -1,8 +0,0 @@
-using OurTube.Domain.Entities;
-
-namespace OurTube.Domain.Interfaces;
-
-public interface ITagRepository:IRepository<Tag>
-{
-    Task<bool> ContainsAsync(string name);
-}
