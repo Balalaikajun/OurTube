1e440c65e570d9f6168851d9609d10255f87c818
Andreu K.P. <andrejkluev774@gmail.com>
2025-06-13 17:15:10 +0700

    Update commentary add and get logic

--- backend/src/OurTube.Application/DTOs/Comment/CommentPostDto.cs
+++ backend/src/OurTube.Application/DTOs/Comment/CommentPostDto.cs
@@ -4,5 +4,5 @@ public class CommentPostDto
 {
     public int VideoId { get; set; }
     public string Text { get; set; }
-    public int? ParentId { get; set; }
+    public int? ParentId { get; set; } = null;
 }
